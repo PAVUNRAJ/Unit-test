479d7497efff00fdf83ba7de8b7d6ce5fd44444a
PAVUNRAJ <pavunrajtech@gmail.com>
2024-03-05 11:30:15 +0530

    Initial commit

---
+++ Bookmyshow/Bookmyshow/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookmyshow
+//
+//  Created by PavunRaj on 01/03/24.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bookmyshow/Bookmyshow/Model/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Bookmyshow
+//
+//  Created by PavunRaj on 01/03/24.
+//
+
+import Foundation
